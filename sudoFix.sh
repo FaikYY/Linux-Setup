60074decd088084689b941d354ac75d976cd1703
FaikYY <yazilimcifaik@gmail.com>
2020-10-08 01:18:46 -0400

    added sleep command and a guide to inform user

--- sudoFix.sh
+++ sudoFix.sh
@@ -2,16 +2,31 @@
 
 
 #NOTE: Since we do not have the root access, this script is supposed to run as root by using command "su"
+echo "Welcome to sudoers fixer script, sit back on relax. I'm gonna take care of everything for you :)"
+sleep .5
+
 
 # go to superuser mode
 #[ "${USER:-}" = "root" ] || exec sudo "$0" "$@"
 
 # check updates
+echo "apt-get update is running.."
+sleep 1
 apt-get update 
 
 # if exist install
+echo "apt-get upgrade is running.."
+sleep 1
 apt-get upgrade
 
 # install needed programs
+echo "crucial packages are being download.."
+sleep 1
 apt-get install build-essential module-assistant
 
+# now we're ready to rock
+echo "superuser is being fixed, please wait a second.."
+sleep 1
+apt-get install sudo -y
+usermod -aG sudo syreus
+chmod 0440 /etc/sudoers
