d8222ac50ff00239e11904892ef528717a7aa7d0
FaikYY <yazilimcifaik@gmail.com>
2020-10-08 02:20:42 -0400

    added new instructions

--- sudoFix.sh
+++ sudoFix.sh
@@ -2,11 +2,11 @@
 
 
 #NOTE: Since we do not have the root access, this script is supposed to run as root by using command "su"
-echo "WELCOME TO SUDOERS-FILE-FIX SCRIPT, SIT BACK AND RELAX. I'M GONNA TAKE CARE OF EVERYTHING FOR YOU :)"
-sleep 2.5
+echo "####  WELCOME TO SUDOERS-FILE-FIX SCRIPT, SIT BACK AND RELAX. I'M GONNA TAKE CARE OF EVERYTHING FOR YOU :)  ####"
+sleep 4.5
 
 if [ "$EUID" -ne 0 ]
-  then echo "This script must be run as ROOT user. PLease try 'su' command and run again.. "
+  then echo "!!!!!!!!!   This script must be run as ROOT user. PLease try 'su -' command and run again..  !!!!!!!!!! "
   exit
 fi
 
@@ -15,24 +15,26 @@ fi
 #[ "${USER:-}" = "root" ] || exec sudo "$0" "$@"
 
 # check updates
-echo "apt-get update is running.."
-sleep 2
+echo "**********  sudo-apt update is running..  **********"
+sleep 3
 apt-get update 
 
 # if exist install
-echo "apt-get upgrade is running.."
-sleep 2
+echo "**********  apt-get upgrade is running..  **********"
+sleep 3
 apt-get upgrade
 
 # install needed programs
-echo "crucial packages are being downloaded.."
-sleep 2.5
+echo "**********   crucial packages are being downloaded..  **********"
+sleep 3
 apt-get install build-essential module-assistant
 
 # now we're ready to rock
-echo "superuser is being fixed, please wait a second.."
-sleep 2
+echo "**********   superuser is being fixed, please wait a second..  ********** "
+sleep 3
 apt-get install sudo -y
 usermod -aG sudo syreus
 chmod 0440 /etc/sudoers
+
+echo "**********  !!COMPLETED SUCCESSFULLY, PLEASE REBOOT TO SEE THE CHANGES!!  **********"
 
