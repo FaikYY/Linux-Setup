042ab110cac64926dce889b9c0cab8197c4ef3b6
FaikYY <yazilimcifaik@gmail.com>
2020-10-08 08:19:33 -0400

    sleep time variable added

--- scripts/sudoFix.sh
+++ scripts/sudoFix.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
+#################
+##  variables  ##
+#################
+sleep_time=4
 
 #NOTE: Since we do not have the root access, this script is supposed to run as root by using command "su"
 echo "####  WELCOME TO SUDOERS-FILE-FIX SCRIPT, SIT BACK AND RELAX. I'M GONNA TAKE CARE OF EVERYTHING FOR YOU :)  ####"
-sleep 4.5
+sleep $sleep_time
 
 if [ "$EUID" -ne 0 ]
   then echo "!!!!!!!!!   This script must be run as ROOT user. PLease try 'su -' command and run again..  !!!!!!!!!! "
@@ -16,22 +20,22 @@ fi
 
 # check updates
 echo "**********  sudo-apt update is running..  **********"
-sleep 3
+sleep $sleep_time
 apt-get update 
 
 # if exist install
 echo "**********  apt-get upgrade is running..  **********"
-sleep 3
+sleep $sleep_time
 apt-get upgrade
 
 # install needed programs
 echo "**********   crucial packages are being downloaded..  **********"
-sleep 3
+sleep $sleep_time
 apt-get install build-essential module-assistant
 
 # now we're ready to rock
 echo "**********   superuser is being fixed, please wait a second..  ********** "
-sleep 3
+sleep $sleep_time
 apt-get install sudo -y
 usermod -aG sudo syreus
 chmod 0440 /etc/sudoers
