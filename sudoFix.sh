0bd30e5303c56863724766c476c9950b3747866b
FaikYY <yazilimcifaik@gmail.com>
2020-10-08 01:11:08 -0400

    removed unnecessary code, run this version as root by using su command

--- sudoFix.sh
+++ sudoFix.sh
@@ -1,14 +1,17 @@
-# go to main directory
-cd
+#! /bin/bash
+
+
+#NOTE: Since we do not have the root access, this script is supposed to run as root by using command "su"
 
 # go to superuser mode
-su
+#[ "${USER:-}" = "root" ] || exec sudo "$0" "$@"
 
 # check updates
-apt-get update
+apt-get update 
 
 # if exist install
 apt-get upgrade
 
 # install needed programs
 apt-get install build-essential module-assistant
+
