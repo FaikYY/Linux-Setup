915d63a3933441f39f2d56e614be3b7278fc9a55
FaikYY <yazilimcifaik@gmail.com>
2020-10-08 09:55:54 -0400

    minor stuff

--- scripts/packageInstallerScripts/bspwmAndSxhkdInstaller.sh
+++ scripts/packageInstallerScripts/bspwmAndSxhkdInstaller.sh
@@ -6,7 +6,7 @@
 sleep_time=4
 
 #NOTE: Since we do not have the root access, this script is supposed to run as root by using command "su"
-echo "####  WELCOME TO SUDOERS-FILE-FIX SCRIPT, SIT BACK AND RELAX. I'M GONNA TAKE CARE OF EVERYTHING FOR YOU :)  ####"
+echo "####  WELCOME TO BSPWM AND SXHKD INSTALLER SCRIPT, SIT BACK AND RELAX. I'M GONNA TAKE CARE OF EVERYTHING FOR YOU :)  ####"
 sleep $sleep_time
 
 if [ "$EUID" -ne 0 ]
@@ -17,10 +17,10 @@ fi
 # install dependencies
 echo "***********  dependencies are installing  **************"
 sleep $sleep_time
-sudo apt-get install libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev
+sudo apt-get install -y libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev
 
 # go ahead and install 
-echo "***********  bspwm and sxhkd github repos installing"
+echo "***********  bspwm and sxhkd github repos installing  ***********"
 sleep $sleep_time
 git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
@@ -29,9 +29,9 @@ cd ../sxhkd && make && sudo make install
 
 echo "********** building from source and placing configs  *********** "
 sleep $sleep_time
-mkdir -p ~/.config/bspwm && mkdir -p ~/.config/sxhkd
-cp ~/linuxSetup/configArchive/bspwm/bspwmrc ~/.config/bspwm
-cp ~/linuxSetup/configArchive/sxhkd/sxhkdrc ~/.config/sxhkd
-chmod u+x ~/.config/bspwm/bspwmrc
+mkdir -p /home/syreus/.config/bspwm && mkdir -p /home/syreus/.config/sxhkd
+cp /home/syreus/linuxSetup/configArchive/bspwm/bspwmrc /home/syreus/.config/bspwm
+cp /home/syreus/linuxSetup/configArchive/sxhkd/sxhkdrc /home/syreus/.config/sxhkd
+chmod u+x /home/syreus/.config/
 
 echo "!!!!!!!!!!  script completed successfully, you can reboot now  !!!!!!!!!!!"
