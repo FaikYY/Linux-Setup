00edac1b1d909be7a4a28e48ec6ccb31d8b25344
FaikYY <yazilimcifaik@gmail.com>
2020-10-08 01:21:07 -0400

    fixed minor bugs

--- sudoFix.sh
+++ sudoFix.sh
@@ -3,7 +3,7 @@
 
 #NOTE: Since we do not have the root access, this script is supposed to run as root by using command "su"
 echo "Welcome to sudoers fixer script, sit back on relax. I'm gonna take care of everything for you :)"
-sleep .5
+sleep 1.5
 
 
 # go to superuser mode
@@ -11,22 +11,22 @@ sleep .5
 
 # check updates
 echo "apt-get update is running.."
-sleep 1
+sleep 1.5
 apt-get update 
 
 # if exist install
 echo "apt-get upgrade is running.."
-sleep 1
+sleep 1.5
 apt-get upgrade
 
 # install needed programs
-echo "crucial packages are being download.."
-sleep 1
+echo "crucial packages are being downloaded.."
+sleep 1.5
 apt-get install build-essential module-assistant
 
 # now we're ready to rock
 echo "superuser is being fixed, please wait a second.."
-sleep 1
+sleep 1.5
 apt-get install sudo -y
 usermod -aG sudo syreus
 chmod 0440 /etc/sudoers
